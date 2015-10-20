1c13497dbf9e54e9b7f5935c48c4105bc3ee58db
DonChatelain <djchatelain@gmail.com>
2015-10-20 16:28:04 -0700

    Puzzle 8,

---
+++ JavaScript/puzzle8.js
@@ -0,0 +1,47 @@
+var buttonRight = document.getElementById('correct-button');
+var buttonGo = document.getElementById('go-button');
+var wrongA = document.getElementById('wrong-a');
+var wrongB = document.getElementById('wrong-b');
+var wrongC = document.getElementById('wrong-c');
+var resCorrect = document.getElementById('res-correct');
+var resWrong = document.getElementById('res-wrong');
+
+buttonRight.addEventListener('click', function(e){
+  e.preventDefault();
+  resCorrect.className = 'visible';
+  buttonGo.className = 'visible';
+  buttonRight.className = 'hidden';
+  wrongA.className = 'hidden';
+  wrongB.className = 'hidden';
+  wrongC.className = 'hidden';
+});
+
+buttonGo.addEventListener('click', function(){
+  window.location.href="index.html";
+});
+
+wrongA.addEventListener('click', function(e){
+  e.preventDefault();
+  resWrong.className = 'visible';
+  buttonRight.className = 'hidden';
+  wrongA.className = 'hidden';
+  wrongB.className = 'hidden';
+  wrongC.className = 'hidden';
+});
+wrongB.addEventListener('click', function(e){
+  e.preventDefault();
+  resWrong.className = 'visible';
+  buttonRight.className = 'hidden';
+  wrongA.className = 'hidden';
+  wrongB.className = 'hidden';
+  wrongC.className = 'hidden';
+});
+wrongC.addEventListener('click', function(e){
+  e.preventDefault();
+  resWrong.className = 'visible';
+  buttonRight.className = 'hidden';
+  wrongA.className = 'hidden';
+  wrongB.className = 'hidden';
+  wrongC.className = 'hidden';
+});
+
