e9cc9555bebe700c1223eb2179340e40b05fe2d8
Natalie Chow <cychownat@gmail.com>
2015-10-22 11:37:30 -0700

    Disabled buttons, changed timer funtionality

--- JavaScript/puzzle8.js
+++ JavaScript/puzzle8.js
@@ -1,67 +1,59 @@
-var buttonRight = document.getElementById('correct-button');
-var buttonGo = document.getElementById('cont');
-var wrongA = document.getElementById('wrong-a');
-var wrongB = document.getElementById('wrong-b');
-var wrongC = document.getElementById('wrong-c');
-var resCorrect = document.getElementById('res-correct');
-var resWrong = document.getElementById('res-wrong');
-var resContain = document.getElementById('res-contain');
-
 var currentStage;
 getLocal();
 
 if (currentStage < 8) {
   window.location.href = 'index.html';
-  console.log("Cheater");
+  alert('Cheater');
 } else {
 
-  // timerAll( 90, 'res-contain');
+  var buttonRight = document.getElementById('correct-button');
+  var buttonGo = document.getElementById('cont');
+  var wrongA = document.getElementById('wrong-a');
+  var wrongB = document.getElementById('wrong-b');
+  var wrongC = document.getElementById('wrong-c');
+  var resCorrect = document.getElementById('res-correct');
+  var resWrong = document.getElementById('res-wrong');
+
+  var disableButtons = function() {
+    buttonRight.disabled = true;
+    wrongA.disabled = true;
+    wrongB.disabled = true;
+    wrongC.disabled = true;
+  }
+
+  timerAll(90);
 
   buttonRight.addEventListener('click', function(e){
     e.preventDefault();
     localize(9);
-
     resCorrect.className = 'visible';
-    buttonGo.className = 'visible';
-    buttonRight.className = 'hidden';
-    wrongA.className = 'hidden';
-    wrongB.className = 'hidden';
-    wrongC.className = 'hidden';
+    disableButtons();
+    stopTimer();
+  });
+
+  wrongA.addEventListener('click', function(e){
+    e.preventDefault();
+    resWrong.className = 'visible';
+    disableButtons();
+    stopTimer();
+  });
+  wrongB.addEventListener('click', function(e){
+    e.preventDefault();
+    resWrong.className = 'visible';
+    disableButtons();
+    stopTimer();
+  });
+  wrongC.addEventListener('click', function(e){
+    e.preventDefault();
+    resWrong.className = 'visible';
+    disableButtons();
+    stopTimer();
   });
 
   buttonGo.addEventListener('click', function(e){
     e.preventDefault();
     window.location.href="puzzle9.html";
   });
-
-  wrongA.addEventListener('click', function(e){
-    e.preventDefault();
-
-    resWrong.className = 'visible';
-    buttonRight.className = 'hidden';
-    wrongA.className = 'hidden';
-    wrongB.className = 'hidden';
-    wrongC.className = 'hidden';
-  });
-  wrongB.addEventListener('click', function(e){
-    e.preventDefault();
-
-    resWrong.className = 'visible';
-    buttonRight.className = 'hidden';
-    wrongA.className = 'hidden';
-    wrongB.className = 'hidden';
-    wrongC.className = 'hidden';
-  });
-  wrongC.addEventListener('click', function(e){
-    e.preventDefault();
-
-    resWrong.className = 'visible';
-    buttonRight.className = 'hidden';
-    wrongA.className = 'hidden';
-    wrongB.className = 'hidden';
-    wrongC.className = 'hidden';
-  });
-
   document.getElementById('return-button').addEventListener('click', function(e){
     event.preventDefault();
     window.location.href = 'Fail.html';
