1aac06feccb0b3782f976ff0265753ba55e10d1c
Natalie Chow <cychownat@gmail.com>
2015-10-21 20:37:48 -0700

    Updated puzzles 7 & 10, fixed glitch in universal CSS

--- JavaScript/puzzle4.js
+++ JavaScript/puzzle4.js
@@ -7,24 +7,24 @@ if (currentStage < 4) {
 } else {
   // timerAll(90, 'res-contain');
 
-  var ansTrue = document.getElementById('ans-true');
-  var ansFalse = document.getElementById('ans-false');
+  var trueButton = document.getElementById('ans-true');
+  var falseButton = document.getElementById('ans-false');
 
   // event listener for correct answer
-  ansFalse.addEventListener('click', function(e){
+  falseButton.addEventListener('click', function(e){
     e.preventDefault();
     localize(5);
     document.getElementById('res-correct').className = 'visible';
-    ansTrue.disabled = true;
-    ansFalse.disabled = true;
+    trueButton.disabled = true;
+    falseButton.disabled = true;
   });
 
   // event listener for wrong answer
-  ansTrue.addEventListener('click', function(e){
+  trueButton.addEventListener('click', function(e){
     e.preventDefault();
     document.getElementById('res-wrong').className = 'visible';
-    ansTrue.disabled = true;
-    ansFalse.disabled = true;
+    trueButton.disabled = true;
+    falseButton.disabled = true;
   });
 
   document.getElementById('cont').addEventListener('click', function(){
@@ -35,4 +35,3 @@ if (currentStage < 4) {
     window.location.href = 'Fail.html';
   });
 }
-
