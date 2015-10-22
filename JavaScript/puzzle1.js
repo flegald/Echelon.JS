ded0590efd51e699b074478a520e6ca3ae8b8841
DonChatelain <djchatelain@gmail.com>
2015-10-22 11:35:52 -0700

    adds full timer functionality in PUZZLE 1, adds disable button in PUZZLE 1

--- JavaScript/puzzle1.js
+++ JavaScript/puzzle1.js
@@ -1,32 +1,41 @@
+var currentStage;
+getLocal();
+
 var wroAns = document.getElementById('ans-True');
 var corAns = document.getElementById('ans-False');
 var showCor = document.getElementById('res-correct');
 var showWro = document.getElementById('res-wrong');
-var cont =  document.getElementById('cont')
+var cont =  document.getElementById('cont');
 
-var currentStage;
-getLocal();
+var disableButtons = function() {
+  wroAns.disabled = true;
+  corAns.disabled = true;
+};
 
-
-// timerAll(90,'end');
+timerAll(90);
 
 corAns.addEventListener('click', function(e){
-	event.preventDefault();
+	e.preventDefault();
 	showCor.className = ('visible');
   localize(2);
-	});
-
+  disableButtons();
+  stopTimer();
+});
 
 wroAns.addEventListener('click', function(e){
-	event.preventDefault();
+	e.preventDefault();
 	showWro.className = ('visible');
+  disableButtons();
+  stopTimer();
 });
 
 document.getElementById('return-button').addEventListener('click', function(e){
-	window.location = 'Fail.html';
+	e.preventDefault();
+  window.location = 'Fail.html';
 });
 
 cont.addEventListener('click', function(e){
+  e.preventDefault();
   window.location = "puzzle2.html";
 });
 
