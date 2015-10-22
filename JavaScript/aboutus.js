82ef80b7eff64f22022f119bbfafc128b22b3bee
Mike <mikekh@uw.edu>
2015-10-21 17:03:33 -0700

    added a js file for about us

---
+++ JavaScript/aboutus.js
@@ -0,0 +1,9 @@
+var agenta = document.getElementById('agenta');
+var agent1 = document.getElementById('agent1');
+
+dec1.addEventListener('click', function(e) {
+  event.preventDefault();
+  agent1.className="hidden";
+  agenta.className="visible";
+
+});
