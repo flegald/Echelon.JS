80b256a5190b71820aa13d84a0e24ee229c7fdac
DonChatelain <djchatelain@gmail.com>
2015-10-22 17:18:04 -0700

    inserted vault animation in success page

--- JavaScript/success.js
+++ JavaScript/success.js
@@ -1,23 +1,47 @@
 var door = document.getElementById('door');
 var prize = document.getElementById('prize');
-var open = document.getElementById('open');
-var vdoor = document.getElementById('vaultdoor');
 
 var tempName = localStorage.getItem('AgentName');
 var tempAlias = localStorage.getItem('AgentAlias');
 var userName = JSON.parse(tempName);
 var userAlias = JSON.parse(tempAlias);
 
-
 var putAlias = document.getElementById('putAlias');
 
 var writeAlias = function() {
-  putAlias.innerHTML=userAlias;
+  putAlias.innerHTML = userAlias;
 };
 
 writeAlias();
-vdoor.addEventListener('click', function(e){
-  door.className ="hidden";
-  prize.className ="visible";
-});
 
+$('#door').on('click', function() {
+
+    var $center = $("#center"), degree = 0, timer;
+    rotate();
+    function rotate() {
+
+        $center.css({ '-webkitTransform': 'rotate(' + degree + 'deg)'});
+        $center.css({ '-moz-transform': 'rotate(' + degree + 'deg)'});
+        timer = setTimeout(function() {
+            ++degree; rotate();
+        },10);
+    }
+
+    setTimeout(function() {
+        clearTimeout(timer);
+        $('#right').animate({
+            left: '+=200px'
+        }, 2000);
+        $('#left').animate({
+            left: '-=200px'
+        }, 2000);
+        $('#center').animate({
+            left: '+=200px'
+        }, 2000);
+    }, 2000);
+
+  setTimeout(function(){
+    prize.className = 'visible';
+  }, 4000);
+
+});
